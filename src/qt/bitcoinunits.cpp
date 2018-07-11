f0e9fbd80bf83f519f5b8921efe65a075b7bc2e2
Chaozer <chaozer@swippcoin.com>
2018-07-11 12:28:40 +0000

    Clean up qt/bitcoinunits.ccp/h and add copyright headers

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2017-2018 The Swipp developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "bitcoinunits.h"
 
 #include <QStringList>
@@ -11,9 +17,11 @@ BitcoinUnits::BitcoinUnits(QObject *parent):
 QList<BitcoinUnits::Unit> BitcoinUnits::availableUnits()
 {
     QList<BitcoinUnits::Unit> unitlist;
+
     unitlist.append(BTC);
     unitlist.append(mBTC);
     unitlist.append(uBTC);
+
     return unitlist;
 }
 
@@ -67,9 +75,9 @@ int BitcoinUnits::amountDigits(int unit)
 {
     switch(unit)
     {
-    case BTC: return 8; // 21,000,000 (# digits, without commas)
-    case mBTC: return 11; // 21,000,000,000
-    case uBTC: return 14; // 21,000,000,000,000
+    case BTC: return 8;    // 14,000,000 (# digits, without commas)
+    case mBTC: return 11;  // 14,000,000,000
+    case uBTC: return 14;  // 14,000,000,000,000
     default: return 0;
     }
 }
@@ -87,10 +95,10 @@ int BitcoinUnits::decimals(int unit)
 
 QString BitcoinUnits::format(int unit, qint64 n, bool fPlus)
 {
-    // Note: not using straight sprintf here because we do NOT want
-    // localized number formatting.
+    // Note: not using straight sprintf here because we do NOT want localized number formatting
     if(!valid(unit))
         return QString(); // Refuse to format invalid unit
+
     qint64 coin = factor(unit);
     int num_decimals = decimals(unit);
     qint64 n_abs = (n > 0 ? n : -n);
@@ -101,14 +109,17 @@ QString BitcoinUnits::format(int unit, qint64 n, bool fPlus)
 
     // Right-trim excess zeros after the decimal point
     int nTrim = 0;
+
     for (int i = remainder_str.size()-1; i>=2 && (remainder_str.at(i) == '0'); --i)
         ++nTrim;
+
     remainder_str.chop(nTrim);
 
     if (n < 0)
         quotient_str.insert(0, '-');
     else if (fPlus && n > 0)
         quotient_str.insert(0, '+');
+
     return quotient_str + QString(".") + remainder_str;
 }
 
@@ -121,36 +132,33 @@ bool BitcoinUnits::parse(int unit, const QString &value, qint64 *val_out)
 {
     if(!valid(unit) || value.isEmpty())
         return false; // Refuse to parse invalid unit or empty string
+
     int num_decimals = decimals(unit);
     QStringList parts = value.split(".");
 
     if(parts.size() > 2)
-    {
         return false; // More than one dot
-    }
+
     QString whole = parts[0];
     QString decimals;
 
     if(parts.size() > 1)
-    {
         decimals = parts[1];
-    }
+
     if(decimals.size() > num_decimals)
-    {
         return false; // Exceeds max precision
-    }
+
     bool ok = false;
     QString str = whole + decimals.leftJustified(num_decimals, '0');
 
     if(str.size() > 18)
-    {
         return false; // Longer numbers will exceed 63 bits
-    }
+
     qint64 retvalue = str.toLongLong(&ok);
+
     if(val_out)
-    {
         *val_out = retvalue;
-    }
+
     return ok;
 }
 
@@ -163,9 +171,11 @@ int BitcoinUnits::rowCount(const QModelIndex &parent) const
 QVariant BitcoinUnits::data(const QModelIndex &index, int role) const
 {
     int row = index.row();
+
     if(row >= 0 && row < unitlist.size())
     {
         Unit unit = unitlist.at(row);
+
         switch(role)
         {
         case Qt::EditRole:
@@ -177,5 +187,6 @@ QVariant BitcoinUnits::data(const QModelIndex &index, int role) const
             return QVariant(static_cast<int>(unit));
         }
     }
+
     return QVariant();
 }
