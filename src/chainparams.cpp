e5fa70e91c3580a64e902191b0b3cb0d680c988c
Chaozer <chaozer@swippcoin.com>
2018-07-12 14:39:06 +0000

    Cleanup of chainparams.cpp/h

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1,10 +1,10 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2017-2018 The Swipp developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "assert.h"
-
 #include "chainparams.h"
 #include "main.h"
 #include "util.h"
@@ -20,31 +20,33 @@ struct SeedSpec6 {
 
 #include "chainparamsseeds.h"
 
-//
-// Main network
-//
-
 // Convert the pnSeeds6 array into usable address objects.
 static void convertSeed6(std::vector<CAddress> &vSeedsOut, const SeedSpec6 *data, unsigned int count)
 {
-    // It'll only connect to one or two seed nodes because once it connects,
-    // it'll get a pile of addresses with newer timestamps.
+    // Will only connect to one or two seed nodes because once it connects,
+    // it will get a pile of addresses with newer timestamps.
     // Seed nodes are given a random 'last seen time' of between one and two
     // weeks ago.
-    const int64_t nOneWeek = 7*24*60*60;
+    const int64_t nOneWeek = 7 * 24 * 60 * 60;
+
     for (unsigned int i = 0; i < count; i++)
     {
         struct in6_addr ip;
         memcpy(&ip, data[i].addr, sizeof(ip));
         CAddress addr(CService(ip, data[i].port));
+
         addr.nTime = GetTime() - GetRand(nOneWeek) - nOneWeek;
         vSeedsOut.push_back(addr);
     }
 }
 
-class CMainParams : public CChainParams {
+// Main network
+
+class CMainParams : public CChainParams
+{
 public:
-    CMainParams() {
+    CMainParams()
+    {
         // The message start string is designed to be unlikely to occur in normal data.
         // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
         // a large 4-byte int at any alignment.
@@ -52,6 +54,7 @@ public:
         pchMessageStart[1] = 0xe2;
         pchMessageStart[2] = 0xc5;
         pchMessageStart[3] = 0x54;
+
         vAlertPubKey = ParseHex("");
         nDefaultPort = 24055;
         nRPCPort = 35075;
@@ -59,20 +62,21 @@ public:
 
         // Build the genesis block. Note that the output of the genesis coinbase cannot
         // be spent as it did not originally exist in the database.
-        //
-        //CBlock(hash=000001faef25dec4fbcf906e6242621df2c183bf232f263d0ba5b101911e4563, ver=1, hashPrevBlock=0000000000000000000000000000000000000000000000000000000000000000, hashMerkleRoot=12630d16a97f24b287c8c2594dda5fb98c9e6c70fc61d44191931ea2aa08dc90, nTime=1393221600, nBits=1e0fffff, nNonce=164482, vtx=1, vchBlockSig=)
-        //  Coinbase(hash=12630d16a9, nTime=1393221600, ver=1, vin.size=1, vout.size=1, nLockTime=0)
-        //    CTxIn(COutPoint(0000000000, 4294967295), coinbase 00012a24323020466562203230313420426974636f696e2041544d7320636f6d6520746f20555341)
-        //    CTxOut(empty)
-        //  vMerkleTree: 12630d16a9
+
         const char* pszTimestamp = "Swipp - The Beginning 5/9/2017";
         std::vector<CTxIn> vin;
+
         vin.resize(1);
-        vin[0].scriptSig = CScript() << 0 << CBigNum(42) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+        vin[0].scriptSig = CScript() << 0 << CBigNum(42) <<
+            vector<unsigned char>((const unsigned char*)pszTimestamp,
+            (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+
         std::vector<CTxOut> vout;
         vout.resize(1);
         vout[0].SetEmpty();
+
         CTransaction txNew(1, 1496692022, vin, vout, 0);
+
         genesis.vtx.push_back(txNew);
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
@@ -81,17 +85,12 @@ public:
         genesis.nBits    = 0x1e0fffff;
         genesis.nNonce   = 1454059;
 
-
-
         hashGenesisBlock = genesis.GetHash();
-        //printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
-        //printf("merkle = %s\n", genesis.hashMerkleRoot.ToString().c_str());
         assert(hashGenesisBlock == uint256("0xe94755b352f86725342f816a148ba490e07df1d7ae3a135fc0632ae4a83f8e81"));
         assert(genesis.hashMerkleRoot == uint256("0x5cd583483a3f368810466dd6ac054dc5bc75b46ef03952e8ed7eefd3b32a8160"));
 
         vSeeds.push_back(CDNSSeedData("node1.swippcoin.com", "node1.swippcoin.com"));
         vSeeds.push_back(CDNSSeedData("node2.swippcoin.com", "node2.swippcoin.com"));
-
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,63);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
@@ -100,46 +99,42 @@ public:
         base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
-
         nLastPOWBlock = 6000000;
     }
 
     virtual const CBlock& GenesisBlock() const { return genesis; }
     virtual Network NetworkID() const { return CChainParams::MAIN; }
+    virtual const vector<CAddress>& FixedSeeds() const { return vFixedSeeds; }
 
-    virtual const vector<CAddress>& FixedSeeds() const {
-        return vFixedSeeds;
-    }
 protected:
     CBlock genesis;
     vector<CAddress> vFixedSeeds;
 };
+
 static CMainParams mainParams;
 
-
-//
 // Testnet
-//
 
-class CTestNetParams : public CMainParams {
+class CTestNetParams : public CMainParams
+{
 public:
-    CTestNetParams() {
-        // The message start string is designed to be unlikely to occur in normal data.
-        // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
-        // a large 4-byte int at any alignment.
+    CTestNetParams()
+    {
         pchMessageStart[0] = 0xe1;
         pchMessageStart[1] = 0xc4;
         pchMessageStart[2] = 0xf8;
         pchMessageStart[3] = 0x21;
+
         bnProofOfWorkLimit = CBigNum(~uint256(0) >> 16);
         vAlertPubKey = ParseHex("");
         nDefaultPort = 18065;
         nRPCPort = 15075;
         strDataDir = "testnet";
 
-        // Modify the testnet genesis block so the timestamp is valid for a later start.
+        // Modify the testnet genesis block so the timestamp is valid for a later start
+
         genesis.nBits  = 0x1e0fffff; //504365055 Decimal Number
-        genesis.nTime    = 1504550001;
+        genesis.nTime  = 1504550001;
         genesis.nNonce = 1454059;
 
         assert(hashGenesisBlock == uint256("0xe94755b352f86725342f816a148ba490e07df1d7ae3a135fc0632ae4a83f8e81"));
@@ -154,22 +149,24 @@ public:
         base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
 
         convertSeed6(vFixedSeeds, pnSeed6_test, ARRAYLEN(pnSeed6_test));
-
         nLastPOWBlock = 0x7fffffff;
     }
+
     virtual Network NetworkID() const { return CChainParams::TESTNET; }
 };
+
 static CTestNetParams testNetParams;
-
-
 static CChainParams *pCurrentParams = &mainParams;
 
-const CChainParams &Params() {
+const CChainParams &Params()
+{
     return *pCurrentParams;
 }
 
-void SelectParams(CChainParams::Network network) {
-    switch (network) {
+void SelectParams(CChainParams::Network network)
+{
+    switch (network)
+    {
         case CChainParams::MAIN:
             pCurrentParams = &mainParams;
             break;
@@ -182,14 +179,14 @@ void SelectParams(CChainParams::Network network) {
     }
 }
 
-bool SelectParamsFromCommandLine() {
-
+bool SelectParamsFromCommandLine()
+{
     bool fTestNet = GetBoolArg("-testnet", false);
 
-    if (fTestNet) {
+    if (fTestNet)
         SelectParams(CChainParams::TESTNET);
-    } else {
+    else
         SelectParams(CChainParams::MAIN);
-    }
+
     return true;
 }
--- src/chainparams.h
+++ src/chainparams.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2013 The Bitcoin developers
+// Copyright (c) 2017-2018 The Swipp developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -20,22 +21,22 @@ typedef unsigned char MessageStartChars[MESSAGE_START_SIZE];
 class CAddress;
 class CBlock;
 
-struct CDNSSeedData {
+struct CDNSSeedData
+{
     string name, host;
     CDNSSeedData(const string &strName, const string &strHost) : name(strName), host(strHost) {}
 };
 
-/**
- * CChainParams defines various tweakable parameters of a given instance of the
- * Bitcoin system. There are three: the main network on which people trade goods
- * and services, the public test network which gets reset from time to time and
- * a regression test mode which is intended for private networks only. It has
- * minimal difficulty to ensure that blocks can be found instantly.
- */
+// Defines various tweakable parameters of a given instance of the
+// Swipp system. There are three: the main network on which people trade goods
+// and services, the public test network which gets reset from time to time and
+// a regression test mode which is intended for private networks only. It has
+// minimal difficulty to ensure that blocks can be found instantly.
 class CChainParams
 {
 public:
-    enum Network {
+    enum Network
+    {
         MAIN,
         TESTNET,
         REGTEST,
@@ -43,7 +44,8 @@ public:
         MAX_NETWORK_TYPES
     };
 
-    enum Base58Type {
+    enum Base58Type
+    {
         PUBKEY_ADDRESS,
         SCRIPT_ADDRESS,
         SECRET_KEY,
@@ -85,23 +87,17 @@ protected:
     int nLastPOWBlock;
 };
 
-/**
- * Return the currently selected parameters. This won't change after app startup
- * outside of the unit tests.
- */
+// Return the currently selected parameters. This won't change after app startup outside of the unit tests
 const CChainParams &Params();
 
-/** Sets the params returned by Params() to those for the given network. */
+// Sets the params returned by Params() to those for the given network
 void SelectParams(CChainParams::Network network);
 
-/**
- * Looks for -regtest or -testnet and then calls SelectParams as appropriate.
- * Returns false if an invalid combination is given.
- */
 bool SelectParamsFromCommandLine();
 
-inline bool TestNet() {
-    // Note: it's deliberate that this returns "false" for regression test mode.
+inline bool TestNet()
+{
+    // NOTE: it's deliberate that this returns "false" for regression test mode.
     return Params().NetworkID() == CChainParams::TESTNET;
 }
 
