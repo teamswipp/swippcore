7d3abee4d84538e7aec2a9bffa1e4eb0086bc30b
Twiddler <twiddler@swippcoin.com>
2018-07-15 00:53:39 +0000

    Fix AppImage loading errors related to libstdc++.so.6
    When compiling with a newer libstdc++, older distributions with an older
    libstdc++ could have trouble loading the AppImage. The remedy is to
    package the library with the AppImage and invoke it when running on an
    older distribution.

--- swipp-linuxdeployqt.sh
+++ swipp-linuxdeployqt.sh
@@ -8,8 +8,10 @@
 # Recipe for creating a Swipp QT AppImage package
 
 # Create AppDir FHS-like stucture
-mkdir -p swipp.AppDir/usr
-mkdir -p swipp.AppDir/usr/bin
+mkdir -p swipp.AppDir/usr swipp.AppDir/usr/bin
+
+# Used by AppImageKit-checkrt (see below)
+mkdir -p swipp.AppDir/usr/optional swipp.AppDir/usr/optional/libstdc++
 
 # Copy files into empty AppDir
 cp swipp-qt swipp.AppDir/usr/bin
@@ -19,15 +21,23 @@ wget -c https://github.com/probonopd/linuxdeployqt/releases/download/continuous/
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 
 # Prepare AppDir
-./linuxdeployqt-continuous-x86_64.AppImage ./swipp.AppDir/usr/bin/swipp-qt
+./linuxdeployqt-continuous-x86_64.AppImage swipp.AppDir/usr/bin/swipp-qt -bundle-non-qt-libs
+
+# Workaround to increase compatibility with older systems; see https://github.com/darealshinji/AppImageKit-checkrt for details
+rm swipp.AppDir/AppRun
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 swipp.AppDir/usr/optional/libstdc++/
+wget -c https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/exec-x86_64.so -O swipp.AppDir/usr/optional/exec.so
+wget -c https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/AppRun-patched-x86_64 -O swipp.AppDir/AppRun
+chmod a+x swipp.AppDir/AppRun
 
 # Copy in desktop descriptor and icon
-printf "[Desktop Entry]\nType=Application\nName=swipp-qt\nGenericName=swipp-qt\nComment=Store and transfer Swipp coins\nIcon=swipp\nExec=/usr/bin/swipp-qt\nTerminal=false\nCategories=Network;Finance;" > swipp.AppDir/swipp-qt.desktop
+printf "[Desktop Entry]\nType=Application\nName=swipp-qt\nGenericName=swipp-qt\nComment=Store and transfer Swipp coins\nIcon=swipp\nExec=../usr/bin/swipp-qt\nTerminal=false\nCategories=Network;Finance;" > swipp.AppDir/swipp-qt.desktop
 cp src/qt/res/icons/swipp.png swipp.AppDir/
 
-# Create executable AppImage
-./linuxdeployqt-continuous-x86_64.AppImage ./swipp.AppDir/usr/bin/swipp-qt -appimage
+# Manually invoke appimagetool so that the modified AppRun stays intact
+./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract
+export PATH=$(readlink -f ./squashfs-root/usr/bin):$PATH
+./squashfs-root/usr/bin/appimagetool swipp.AppDir swipp-qt-x86_64.AppImage
 
 # Purge temporary files
-rm -rf swipp.AppDir linuxdeployqt-continuous-x86_64.AppImage
-
+rm -rf swipp.AppDir linuxdeployqt-continuous-x86_64.AppImage squashfs-root
