1cd52db1e27ebaba37ded161cd296f7bbc73ca4d
Chaozer <chaozer@swippcoin.com>
2018-09-16 21:01:33 +0000

    Cleanup of bitcoind.cpp

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -7,6 +7,7 @@
 #include "rpcserver.h"
 #include "rpcclient.h"
 #include "init.h"
+
 #include <boost/algorithm/string/predicate.hpp>
 
 void WaitForShutdown(boost::thread_group* threadGroup)
@@ -18,6 +19,7 @@ void WaitForShutdown(boost::thread_group* threadGroup)
         MilliSleep(200);
         fShutdown = ShutdownRequested();
     }
+
     if (threadGroup)
     {
         threadGroup->interrupt_all();
@@ -25,10 +27,6 @@ void WaitForShutdown(boost::thread_group* threadGroup)
     }
 }
 
-//////////////////////////////////////////////////////////////////////////////
-//
-// Start
-//
 bool AppInit(int argc, char* argv[])
 {
     boost::thread_group threadGroup;
@@ -36,16 +34,15 @@ bool AppInit(int argc, char* argv[])
 
     try
     {
-        //
-        // Parameters
-        //
         // If Qt is used, parameters/bitcoin.conf are parsed in qt/bitcoin.cpp's main()
         ParseParameters(argc, argv);
+
         if (!boost::filesystem::is_directory(GetDataDir(false)))
         {
             fprintf(stderr, "Error: Specified directory does not exist\n");
             Shutdown();
         }
+
         ReadConfigFile(mapArgs, mapMultiArgs);
 
         if (mapArgs.count("-?") || mapArgs.count("--help"))
@@ -59,44 +56,49 @@ bool AppInit(int argc, char* argv[])
                   "  swippd [options] help <command>      " + _("Get help for a command") + "\n";
 
             strUsage += "\n" + HelpMessage();
-
             fprintf(stdout, "%s", strUsage.c_str());
+
             return false;
         }
 
-        // Command-line RPC
         for (int i = 1; i < argc; i++)
             if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "Swipp:"))
                 fCommandLine = true;
 
         if (fCommandLine)
         {
-            if (!SelectParamsFromCommandLine()) {
+            if (!SelectParamsFromCommandLine())
+            {
                 fprintf(stderr, "Error: invalid combination of -regtest and -testnet.\n");
                 return false;
             }
+
             int ret = CommandLineRPC(argc, argv);
             exit(ret);
         }
+
 #if !defined(WIN32)
         fDaemon = GetBoolArg("-daemon", false);
+
         if (fDaemon)
         {
-            // Daemonize
             pid_t pid = fork();
+
             if (pid < 0)
             {
                 fprintf(stderr, "Error: fork() returned %d errno %d\n", pid, errno);
                 return false;
             }
-            if (pid > 0) // Parent process, pid is child process id
+
+            if (pid > 0)
             {
                 CreatePidFile(GetPidFile(), pid);
                 return true;
             }
-            // Child process falls through to rest of initialization
 
+            // Child process falls through to rest of initialization
             pid_t sid = setsid();
+
             if (sid < 0)
                 fprintf(stderr, "Error: setsid() returned %d errno %d\n", sid, errno);
         }
@@ -104,9 +106,13 @@ bool AppInit(int argc, char* argv[])
 
         fRet = AppInit2(threadGroup);
     }
-    catch (std::exception& e) {
+
+    catch (std::exception& e)
+    {
         PrintException(&e, "AppInit()");
-    } catch (...) {
+    }
+    catch (...)
+    {
         PrintException(NULL, "AppInit()");
     }
 
@@ -116,23 +122,22 @@ bool AppInit(int argc, char* argv[])
         // threadGroup.join_all(); was left out intentionally here, because we didn't re-test all of
         // the startup-failure cases to make sure they don't result in a hang due to some
         // thread-blocking-waiting-for-another-thread-during-startup case
-    } else {
-        WaitForShutdown(&threadGroup);
     }
-    Shutdown();
+    else
+        WaitForShutdown(&threadGroup);
 
+    Shutdown();
     return fRet;
 }
 
 extern void noui_connect();
+
 int main(int argc, char* argv[])
 {
     bool fRet = false;
     fHaveGUI = false;
 
-    // Connect bitcoind signal handlers
     noui_connect();
-
     fRet = AppInit(argc, argv);
 
     if (fRet && fDaemon)
