7ee77e04edfe8cff1439122793e3d3a2ab3d012d
Chaozer <chaozer@swippcoin.com>
2018-09-29 11:30:05 +0000

    Cleanup of eccryptoverify.cpp/h

--- src/eccryptoverify.h
+++ src/eccryptoverify.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
+// Copyright (c) 2017-2018 The Swipp developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -16,6 +17,6 @@ namespace eccrypto {
 bool Check(const unsigned char *vch);
 bool CheckSignatureElement(const unsigned char *vch, int len, bool half);
 
-} // eccrypto namespace
+}
 
 #endif // BITCOIN_ECCRYPTOVERIFY_H
