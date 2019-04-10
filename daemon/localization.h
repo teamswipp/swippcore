dac3fc756c9f93d120905d63e34e740acbbabd84
Chaozer <chaozer@swippcoin.com>
2019-04-10 01:39:32 +0000

    Add localization header
    This will allow us to replace the QT localization currently in use with
    standard GNU gettext() support.

---
+++ daemon/localization.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2017-2019 The Swipp developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef SWIPP_LOCALIZATION_H
+#define SWIPP_LOCALIZATION_H
+
+#include <libintl.h>
+#define _(String) gettext (String)
+
+#endif
