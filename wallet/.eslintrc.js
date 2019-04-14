7999d4cf765ed19b0314ca725f110f31a4823a16
Chaozer <chaozer@swippcoin.com>
2019-04-14 23:21:31 +0000

    Bump ECMAScript to version 8

--- wallet/.eslintrc.js
+++ wallet/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
 
   parser: 'babel-eslint',
   parserOptions: {
-    "ecmaVersion": 6,
+    "ecmaVersion": 8,
     "sourceType": "module",
     "ecmaFeatures": {
       "jsx": true
