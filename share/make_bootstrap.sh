19d0d974149be0596948d31750beaf113cd3e05e
Chaozer <chaozer@swippcoin.com>
2019-04-20 05:16:26 +0000

    Slight rewrite (clean up) of the bootstrap generation script

--- share/make_bootstrap.sh
+++ share/make_bootstrap.sh
@@ -3,29 +3,29 @@
 # Distributed under the MIT/X11 software license, see the accompanying
 # file COPYING.daemon or http://www.opensource.org/licenses/mit-license.php.
 
-RES=$(ps -eo pid,fname | grep swipp)
-PID=${RES% *}
+PID=$(ps -eo pid,fname | grep swipp)
+PID=${PID% *}
 BLOCKFILE_SOURCE_PATTERN=~/.swipp/blk
-BLOCKFILE_DEST=/var/www/html/binaries/bootstrap.dat
+BLOCKFILE_DEST=/var/www/html/binaries/bootstrap
 
 if [ -z "$PID" ]; then
 	exit 1;
 else
 	kill -TSTP $PID
 	sync
-	cat $BLOCKFILE_SOURCE_PATTERN* >| ${BLOCKFILE_DEST}.tmp
+	cat $BLOCKFILE_SOURCE_PATTERN* >| ${BLOCKFILE_DEST}.dat.tmp
 	kill -CONT $PID
 fi
 
-xz -T0 -8 ${BLOCKFILE_DEST}.tmp
+xz -T0 -8 ${BLOCKFILE_DEST}.dat.tmp
 
-RES=$(sha256sum ${BLOCKFILE_DEST}.tmp.xz)
-HASH=${RES% *}
+HASH=$(sha256sum ${BLOCKFILE_DEST}.dat.tmp.xz)
+HASH=${HASH% *}
 
 if [ -z "$PID" ]; then
 	exit 1;
 else
-	cat <(printf $HASH) ${BLOCKFILE_DEST}.tmp.xz > $BLOCKFILE_DEST.bsa
-	rm ${BLOCKFILE_DEST}.tmp.xz
+	cat <(printf $HASH) ${BLOCKFILE_DEST}.dat.tmp.xz > $BLOCKFILE_DEST.bsa
+	rm ${BLOCKFILE_DEST}.dat.tmp.xz
 fi
 
