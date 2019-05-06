a28698160cae058eb0dd2cc4b28e92a88ea16b7f
Chaozer <chaozer@swippcoin.com>
2019-05-06 05:20:41 +0000

    Replace any "-" characters with "." in genbuild.sh

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -18,6 +18,7 @@ if [ -e "$(which git)" ]; then
     # get a string like "v0.6.0-66-g59887e8-dirty"
     DESC="$(git describe --tags 2>/dev/null)"
     DESC=${DESC%???????????}
+    DESC=$(echo $DESC | tr - .)
 
     # get a string like "2012-04-10 16:27:19 +0200"
     TIME="$(git log -n 1 --format="%ci")"
